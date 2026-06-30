426a0512fb03817336016ebc255d4406d7e1b711
Artem <mailforallaccs@gmail.com>
2026-06-30 20:42:51 +0900

    update main.cpp && added a prototype of the App class

---
+++ Manual Map Injector/src/app/app.cpp
@@ -0,0 +1,14 @@
+#include "app.h"
+
+bool App::Init(wchar_t* dll_path, wchar_t* process_name) {
+	// инициализировать logger и syscalls, если надо
+	// инициализировать injector
+	// проверить что длл и процесс корректные
+
+	return true;
+}
+
+int App::Run() {
+	// запустить инжектор и выполнить инъекцию
+	return 0;
+}
--- Manual Map Injector/src/app/app.h
+++ Manual Map Injector/src/app/app.h
@@ -1,5 +1,12 @@
 #pragma once
-class App
-{
-};
+class App {
+public:
+	App() = default;
+	~App() = default;
 
+	bool Init(wchar_t* dll_path, wchar_t* process_name);
+	int Run();
+
+private:
+	// std::unique_ptr<Injector> m_injector;
+};
